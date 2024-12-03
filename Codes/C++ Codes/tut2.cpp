4189dc83e6b719e91056a91a5056044a7d4ddbad
Abhyu-S <abhyudaya410@gmail.com>
2024-12-03 14:43:27 +0530

    abd

--- Codes/C++ Codes/tut2.cpp
+++ Codes/C++ Codes/tut2.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
  using namespace std;
 
+
+void sum(){
+    int glo=5000;
+    cout<<"\n"<<glo;
+    }
  int main(){
+    //integers can also be defined as
+    // int a=5
+    // int b=4
+    int glo=4;
+    glo=10;
     int a=5, b=4;
     float pi=3.14;
     char c = 'j';
-    cout<<a,b,pi,c;
+    cout<<"The value of a is "<<a<<"\nThe Value of b is "<<b;
+    cout<<"\nSum of a and b is "<<a+b;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe character c is "<<c;
+    sum();
+    return 0;
     }
