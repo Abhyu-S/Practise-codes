3cf9189fab26977b4fd9828fe53d1681f67f2240
Abhyu-S <abhyudaya410@gmail.com>
2024-12-03 14:43:16 +0530

    abd

---
+++ Codes/C++ Codes/tut1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// using namespace std;
+
+//This program was created by Abhyudaya
+/*this
+is a 
+multiplies
+line comment */
+int main(){
+    int sum = 6;
+    std::cout<<"Hello World"<< sum;
+    return 0;
+}
---
+++ Codes/C++ Codes/tut2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ using namespace std;
+
+ int main(){
+    int a=5, b=4;
+    float pi=3.14;
+    char c = 'j';
+    cout<<a,b,pi,c;
+    }
