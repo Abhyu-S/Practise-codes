4b391e4cfac896afac2892c6af836678eba94946
Abhyu-S <abhyudaya410@gmail.com>
2024-12-03 15:02:06 +0530

    abd

--- Codes/C++ Codes/tut2.cpp
+++ Codes/C++ Codes/tut2.cpp
@@ -11,14 +11,16 @@ void sum(){
     // int a=5
     // int b=4
     int glo=4;
-    glo=10;
+    glo=45;
     int a=5, b=4;
     float pi=3.14;
     char c = 'j';
+    bool is_true = false;
     cout<<"The value of a is "<<a<<"\nThe Value of b is "<<b;
     cout<<"\nSum of a and b is "<<a+b;
     cout<<"\nThe value of pi is "<<pi;
     cout<<"\nThe character c is "<<c;
     sum();
+    cout<<"\n"<<glo<<"\n"<< is_true;//precedence is given to local variable
     return 0;
     }
