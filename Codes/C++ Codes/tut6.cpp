2709ca48b10d76105ffa143380246477c4d905b8
Abhyu-S <abhyudaya410@gmail.com>
2024-12-04 16:04:25 +0530

    yes

---
+++ Codes/C++ Codes/tut6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip> // involves setw (field width set krne ki allowance hoti hai)
+using namespace std;
+
+int main(){
+    // ********Constants in cpp**********
+    // int a=34;
+    // cout<<"The value of a was: "<<a;
+    // a=45;
+    // cout<<"The value of a now is: "<<a;
+    // const int b=100;
+    // b=13; ham aise change nhi kr sakte coz upar constant likh diye hai
+    
+    //**********manipulators in cpp**********
+    //endl is an example
+    int a=1,b=23,c=234,d=4567;
+    cout<<"The value of a is: "<<a<<endl;
+    cout<<"The value of b is: "<<b<<endl;
+    cout<<"The value of c is: "<<c<<endl;
+    cout<<"The value of d is: "<<d<<endl;
+
+    cout<<"The value of a with setw is: "<<setw(4)<<a<<endl;
+    cout<<"The value of b with setw is: "<<setw(4)<<b<<endl;
+    cout<<"The value of c with setw is: "<<setw(4)<<c<<endl;
+    cout<<"The value of d with setw is: "<<setw(4)<<d<<endl;
+    
+    return 0;
+}
