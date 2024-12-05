b9f7021707a5fb6525af0a6a6bc160343c1a7814
Abhyu-S <abhyudaya410@gmail.com>
2024-12-05 19:15:44 +0530

    yes

--- Codes/C++ Codes/tut7.cpp
+++ Codes/C++ Codes/tut7.cpp
@@ -3,4 +3,23 @@
 // 1. Sequence structure
 // 2. Selection  structure
 // 3. Loop structure
-#include <iostream>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Tell me your age"<<endl;
+    cin>>age;
+    if((age<18) && (age>0)){
+        cout<<"You are not invited to my party";
+    }
+    else if(age<1){
+        cout<<"You are not born yet";
+    }
+    else if(age==18){
+        cout<<"You are a kid, you will be provided with kid pass";
+    }
+    else{
+        cout<<"You are invited to my party";
+    }
+}
