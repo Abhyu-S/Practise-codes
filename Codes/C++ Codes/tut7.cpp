1fe224f6848f6067f4cf722d673761fd39e00f6c
Abhyu-S <abhyudaya410@gmail.com>
2024-12-05 19:28:07 +0530

    done

--- Codes/C++ Codes/tut7.cpp
+++ Codes/C++ Codes/tut7.cpp
@@ -7,19 +7,38 @@
 using namespace std;
 
 int main(){
-    int age;
-    cout<<"Tell me your age"<<endl;
-    cin>>age;
-    if((age<18) && (age>0)){
-        cout<<"You are not invited to my party";
+     int age;
+     cout<<"Tell me your age"<<endl;
+     cin>>age;
+    // if((age<18) && (age>0)){
+    //     cout<<"You are not invited to my party";
+    // }
+    // else if(age<1){
+    //     cout<<"You are not born yet";
+    // }
+    // else if(age==18){
+    //     cout<<"You are a kid, you will be provided with kid pass";
+    // }
+    // else{
+    //     cout<<"You are invited to my party";
+    // }
+
+    //Selection control structure: switch case statements
+    switch (age)
+    {
+    case 18:
+        cout<<"You are 18"<<endl;
+        break;// If break nhi likhenge to jis case me honge us case ke niche wali sari statements print ho jayengi
+    case 2:
+        cout<<"You are 2"<<endl;
+        break;// If break nhi likhenge to jis case me honge us case ke niche wali sari statements print ho jayengi
+    case 22:
+        cout<<"You are 22"<<endl<<endl;
+        break;// If break nhi likhenge to jis case me honge us case ke niche wali sari statements print ho jayengi
+    default:
+        cout<<"No special cases"<<endl;
+        break;// If break nhi likhenge to jis case me honge us case ke niche wali sari statements print ho jayengi
     }
-    else if(age<1){
-        cout<<"You are not born yet";
-    }
-    else if(age==18){
-        cout<<"You are a kid, you will be provided with kid pass";
-    }
-    else{
-        cout<<"You are invited to my party";
-    }
+    cout<<"done with switch case"<<endl;
+    return 0;   
 }
