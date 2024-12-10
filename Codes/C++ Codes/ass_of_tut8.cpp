1824070d46d3c2788657cb8ca0738da9224309d5
Abhyu-S <abhyudaya410@gmail.com>
2024-12-10 15:20:18 +0530

    yra

--- Codes/C++ Codes/ass_of_tut8.cpp
+++ Codes/C++ Codes/ass_of_tut8.cpp
@@ -5,11 +5,16 @@ int main(){
     int num;
     cout<<"Enter the number whose multiplication table you want"<<endl;
     cin>>num;
+    cout<<endl;
     int i=1;
+    if (num>0){
     do{
-        cout<<num*i<<endl;
+        cout<<num<<" x "<<i<<" = "<<num*i<<endl;
         i++;
     }while(i<11);
-
+    }
+    else{
+        cout<<"Enter a Natural number";
+    }
     return 0;
 }
