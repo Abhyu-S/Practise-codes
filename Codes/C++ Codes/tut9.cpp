ef6eb920d3f3140ff3fa3df54430e1dafc1238f5
Abhyu-S <abhyudaya410@gmail.com>
2024-12-12 13:50:05 +0530

    yeah

--- Codes/C++ Codes/tut9.cpp
+++ Codes/C++ Codes/tut9.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+    // for (int i = 0; i < 40; i++)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+    //     if (i==2){
+    //         break;//wahi ke wahi exit krwa dega loop se agar cout break krne ke bad likhu to 1 tak print hoga only
+    //     }
+    // }
     
+    for (int i = 0; i < 6; i++)
+    {
+        /* code */
+        
+        if (i==2){
+            continue;//for only that iteration continue ke niche likhe hue sare statements mat run karo and go to next iteration
+        }
+        cout<<i<<endl;
+    }
     return 0;
 }
