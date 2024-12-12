f9e7f4d2d0e2586f3798d8e527ce7834016ca171
Abhyu-S <abhyudaya410@gmail.com>
2024-12-12 14:47:27 +0530

    hii

---
+++ Codes/C++ Codes/tut10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //What is a pointer?
+    //A data type which holds the address of other data types
+
+    int a=3;
+    int* b = &a; // int* aisa variable jo integer ko point krta ho
+    // & --> Address of operator
+    
+
+    // * --> Dereference operator
+
+    return 0;
+}
