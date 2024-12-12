37e7643e3c35fdbe6b4ea4abcc294ba2a9017a85
Abhyu-S <abhyudaya410@gmail.com>
2024-12-12 14:53:13 +0530

    pointers

--- Codes/C++ Codes/tut10.cpp
+++ Codes/C++ Codes/tut10.cpp
@@ -7,10 +7,12 @@ int main(){
 
     int a=3;
     int* b = &a; // int* aisa variable jo integer ko point krta ho
-    // & --> Address of operator
-    
+    // & --> (Address of) operator. &a means address of a
+    cout<<"Address of a with b is "<<b<<endl;
+    cout<<"Address of a is "<<&a<<endl;
 
     // * --> Dereference operator
+    cout<<"The value at address b is "<<*b<<endl;
 
     return 0;
 }
