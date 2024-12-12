2110ade46f55a6c7b6d0640def2141d22c1f9ce5
Abhyu-S <abhyudaya410@gmail.com>
2024-12-12 18:15:40 +0530

    eee

--- Codes/C++ Codes/tut10.cpp
+++ Codes/C++ Codes/tut10.cpp
@@ -20,6 +20,6 @@ int main(){
     cout<<"The address of b is "<<c<<endl;
     cout<<"The value at address c is "<<*c<<endl;
     cout<<"The value at address value at address value_at(value_at(c)) is "<<**c<<endl;
-    cout<<;
+
     return 0;
 }
