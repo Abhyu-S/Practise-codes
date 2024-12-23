0c06764097bd9fd25d52f9d7575b02551f9209d0
Abhyu-S <abhyudaya410@gmail.com>
2024-12-23 20:56:35 +0530

    sasd

---
+++ Codes/C++ Codes/for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"How many niggers you want? "<<endl;
+    cin>>n;
+    for(int i=1; i<=n;i++){
+        cout<<n<<" niggers !";
+    }
+}
