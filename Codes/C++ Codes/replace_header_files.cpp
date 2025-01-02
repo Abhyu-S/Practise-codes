e7ea49938a87f5bc92eb7a3da9d9df29ed5eb86c
Abhyu-S <abhyudaya410@gmail.com>
2025-01-02 14:50:10 +0530

    agree

---
+++ Codes/C++ Codes/replace_header_files.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<deque>
+#include<stack>
+#include<bitset>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<utility>
+#include<sstream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+#include<memory.h>
+#include<cassert>
+
+//in sab ki jagah likh sakte ho
+
+#include <bits/stdc++.h>
