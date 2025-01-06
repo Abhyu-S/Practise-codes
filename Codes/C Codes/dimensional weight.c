ed1e7ada3eff8eb3a7e2632aea91a367e3737a27
Abhyu-S <abhyudaya410@gmail.com>
2025-01-06 14:02:11 +0530

    comment line

--- Codes/C Codes/dimensional weight.c
+++ Codes/C Codes/dimensional weight.c
@@ -7,5 +7,6 @@ int main(void){
     width = 8;
     volume = length*width*height;
 
-    printf("The dimensional weight of the box is : %d", volume/166);
-}
+    printf("The dimensional weight of the box is : %d", (volume+165)/166); // if it comes out to be 5.734 w/0 adding 165 this will 
+}//return 5, but it is very much above it and should be charged just like that, if the value is just greater than an interger, it equals
+//the next integer
