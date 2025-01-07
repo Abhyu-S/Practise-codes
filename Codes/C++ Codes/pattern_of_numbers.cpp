40914f217d22e0b09b472628541d267cb851fdc2
Abhyu-S <abhyudaya410@gmail.com>
2025-01-07 13:54:16 +0530

    i  did

--- Codes/C++ Codes/pattern_of_numbers.cpp
+++ Codes/C++ Codes/pattern_of_numbers.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void printPattern(int n) {
+void printPattern(int n)
+{
     int size = 2 * n - 1;
     int matrix[size][size];
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             int minDistance = min(min(i, j), min(size - i - 1, size - j - 1));
             matrix[i][j] = n - minDistance;
         }
     }
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter a number: ";
     cin >> n;
