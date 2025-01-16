422172ef7a3642b460bb48c2117ca2ea6ee7dae2
Abhyu-S <abhyudaya410@gmail.com>
2025-01-16 11:07:01 +0530

    yo

--- Codes/C++ Codes/test.cpp
+++ Codes/C++ Codes/test.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> list(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> list[i];
-    }
-
-    vector<int> output;
-    int start = 0, end = n - 1;
-
-    while (start <= end) {
-        if (list[start] > list[end]) {
-            output.push_back(1);
-            --end;
-        } else if (list[start] < list[end]) {
-            output.push_back(2);
-            ++start;
-        } else {
-            output.push_back(0);
-            ++start;
-            --end;
-        }
-    }
-
-    for (int i = 0; i < output.size(); ++i) {
-        cout << output[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+int main(void){
+    string str;
+    // cin.ignore();
+    int i=0;
+    do{
+    getline(cin, str);
+    cout<<str<<endl;
+    i++;
+    }while(i<2);
 }
