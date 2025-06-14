f042dfe7965ac040809c1bd7d4beb9c0d5158ad3
Abhyu-S <abhyudaya410@gmail.com>
2025-06-14 17:36:53 +0530

    tests

--- Codes/C++ Codes/test.cpp
+++ Codes/C++ Codes/test.cpp
@@ -1,48 +1,48 @@
 #include <bits\stdc++.h>
+
 using namespace std;
-const int N = 1e5+10;
-int a[N];
 
-void merge(int l, int r, int mid){
-    int l_sz = mid - l + 1;
-    int r_sz = r - mid;
-    int L[l_sz+1]; int R[r_sz+1];
-    for(int i=0; i<l_sz; i++){
-        L[i] = a[i+l];
-    }
-    for(int i=0; i<r_sz; i++){
-        R[i] = a[i+mid+1];
-    }
-    L[l_sz] = R[r_sz] = INT_MAX;
-    int l_i=0, r_i=0;
-    for(int i=l; i <= r; i++){
-        if(L[l_i]<R[r_i]){
-            a[i] = L[l_i];
-            l_i++;
-        }else{
-            a[i] = R[r_i];
-            r_i++;
+void pushZerosToEnd(vector<int> &arr) {
+        // code here
+        
+        for(int i=0; i<arr.size(); i++){
+            
+            if(arr[i]==0){
+                
+                if(arr[i+1]==0){
+                    // check if its the end
+                    int k;
+                    int flag=1;
+                    for(int j = i+1; j<arr.size(); j++){
+                        if(arr[j] != 0){
+                            k=j;
+                            flag=0;
+                            goto mid;
+                        }
+                    }
+                    if(flag) return;
+                    else{
+                        mid:
+                        arr.erase(arr.begin() + i, arr.begin() + k);
+                        int cnt = k-i;
+                        while(cnt--){
+                            arr.push_back(0);
+                        }
+                    }
+
+                }else{
+                    arr.erase(arr.begin() + i);
+                    arr.push_back(0);
+                }
+            }
         }
-    }
-}
-
-void MergeSort(int l, int r){
-    if(l==r) return;
-    int mid = (l+r)/2;
-    MergeSort(l, mid);
-    MergeSort(mid + 1, r);
-    merge(l, r, mid);
 }
 
 int main(){
-    int n;
-    cin >> n;
-    for(int i=0; i<n; i++){
-        cin >> a[i];
-    }
-    MergeSort(0,n-1);
-    for(int i=0; i<n; i++){
-        cout << a[i] << " ";
+    vector<int> arr = {3 , 5, 0, 0, 4};
+    pushZerosToEnd(arr);
+    for(int x : arr){
+        cout << x << "\t";
     }
     return 0;
 }
